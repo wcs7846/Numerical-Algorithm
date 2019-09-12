d96084493d6e9f77bb6527ad373c9a32dd35dd32
MarkLHF <2751867750@qq.com>
2019-09-12 15:06:43 +0800

    exercise 2 finished

--- Monte-Carlo simulation/Attenuation_collimated_beam.m
+++ Monte-Carlo simulation/Attenuation_collimated_beam.m
@@ -16,14 +16,34 @@
 delta_z = 0.2; % [m]
 a = 1;        % [m-1]
 N = 10000;    % the number of points
-num_photon_vec = zeors(N, 1);
+flag_photon = ones(N,1);
+max_step = 100;
+num_photon_vec = zeros(max_step, 1);
 
-for n = 1:N
-    % move the photon
-    
-    % judging whether or not to be absorbed 
-    dice = rand(1);
-    
+prob = a*delta_z; % the probability of absorption
+% check the validity of prob
+if prob > 1
+    error("[ERROR]:The probability of absorption is over 1!!");
 end
 
-
+for n = 1:max_step
+    % record the number of photon which isnot absorbed
+    num_photon_vec(n) = sum(flag_photon);
+    
+    if num_photon_vec(n) == 0
+        break;
+    end    
+    % move the photon
+    dice = rand(length(flag_photon), 1);
+    % judging whether or not to be absorbed 
+    loc = find(dice<prob);
+    flag_photon(loc) = 0;
+    
+end
+%% show the result 
+figure;
+x_range = (0:max_step-1)*delta_z;% [m]
+bar(x_range, num_photon_vec);hold on; % the distribution of photon
+plot(x_range, num_photon_vec, 'b-');hold on; % the curve
+xlim([0,max_step*delta_z]); ylim([0,N]);
+xlabel('Depth(m)');ylabel('The number of photon');
